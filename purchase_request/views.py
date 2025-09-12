e5d42979b44c7e8eb9c435fc38dc0c033fc30231
Mohammed Dhia Elabed <mstrdhia@gmail.com>
2025-09-12 13:29:19 +0100

    purchase requests by requested_by

--- purchase_request/views.py
+++ purchase_request/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import PurchaseRequest
 from .serializers import PurchaseRequestSerializer
+from rest_framework.response import Response
 
 # Create your views here.
 
 class PurchaseRequestViewSet(viewsets.ModelViewSet):
-    queryset = PurchaseRequest.objects.all()
     serializer_class = PurchaseRequestSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = PurchaseRequest.objects.all()
+        requested_by_id = self.request.query_params.get('requested_by')
+        if requested_by_id:
+            queryset = queryset.filter(requested_by_id=requested_by_id)
+        return queryset
