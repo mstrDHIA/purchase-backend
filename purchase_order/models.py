9f43ca4d95555e625a8504fafe3696db2c7fb43b
Mohammed Dhia Elabed <jasser.boubaker@sesame.com.tn>
2025-12-26 16:03:48 +0100

    a

--- purchase_order/models.py
+++ purchase_order/models.py
@@ -5,6 +5,17 @@ class PurchaseOrder(models.Model):
     id = models.IntegerField(primary_key=True,unique=True)
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
+    statuss = models.CharField(max_length=20, default='pending')
+    #
+    status = models.CharField(max_length=20, 
+    #                           choices=[
+    #     ('pending', 'Pending'),
+    #     ('edited', 'Edited'),
+    #     ('approved', 'Approved'),
+    #     ('rejected', 'Rejected'),
+    #     ('terminated', 'Terminated')
+    # ], 
+    default='pending'),
     approved_by_user = models.ForeignKey(
         'custom_user.User', on_delete=models.CASCADE, related_name='approved_by_user', blank=True, null=True)
     products = models.JSONField(blank=True, null=True)
@@ -12,20 +23,16 @@ class PurchaseOrder(models.Model):
     description = models.TextField()
     requested_by_user = models.ForeignKey(
         'custom_user.User', on_delete=models.CASCADE, related_name='requested_by_user')
-    status = models.CharField(max_length=20, choices=[
-        ('pending', 'Pending'),
-        ('edited', 'Edited'),
-        ('approved', 'Approved'),
-        ('rejected', 'Rejected'),
-        ('terminated', 'Terminated')
-    ], default='pending'),
+    rejected_reason = models.ForeignKey(
+        'reject_reasons.RejectReason', on_delete=models.CASCADE, related_name='rejected_reason', blank=True, null=True)
+    
     currency = models.CharField(max_length=10, blank=True, null=True, default='TND',
                                 choices=[
-('TND', 'TND'),
-('USD', 'USD'),
-('EUR', 'EUR')
+    ('TND', 'TND'),
+    ('USD', 'USD'),
+    ('EUR', 'EUR')
 
-])
+    ])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     priority = models.CharField(max_length=20, choices=[
--- supplier/serializers.py
+++ supplier/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class SupplierSerializer(serializers.ModelSerializer):
     class Meta:
         model = Supplier
-        fields = ['id', 'name', 'contact_email', 'phone_number', 'address', 'created_at', 'updated_at']
+        fields = '__all__'
